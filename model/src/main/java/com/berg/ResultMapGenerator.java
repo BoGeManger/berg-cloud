11c330b247884b19375772e4fd5561bb9e6f4f7f
libg <664120843@qq.com>
2020-08-17 13:31:04 +0800

    扩展校验

--- model/src/main/java/com/berg/ResultMapGenerator.java
+++ model/src/main/java/com/berg/ResultMapGenerator.java
@@ -1,6 +1,6 @@
-package com.berg.dao;
+package com.berg;
 
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.lang.reflect.Field;
 import java.util.Scanner;
