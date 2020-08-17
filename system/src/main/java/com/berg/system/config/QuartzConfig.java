b501a254048d0b7683be9be4e1ff488dcc191203
libg <664120843@qq.com>
2020-08-17 18:19:35 +0800

    no message

---
+++ system/src/main/java/com/berg/system/config/QuartzConfig.java
@@ -0,0 +1,29 @@
+package com.berg.system.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.quartz.SchedulerFactoryBeanCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class QuartzConfig {
+
+    @Autowired
+    private DataSourceProperties dataSourceProperties;
+
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Bean
+    public SchedulerFactoryBeanCustomizer schedulerFactoryBeanCustomizer() {
+        DataSource dataSource = dataSourceProperties.initializeDataSourceBuilder().build();
+        return schedulerFactoryBean -> {
+            schedulerFactoryBean.setDataSource(dataSource);
+            schedulerFactoryBean.setTransactionManager(new DataSourceTransactionManager(dataSource));
+        };
+    }
+}
